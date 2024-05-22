6b4e32e7bdf9fd3fe816a27ed21ccfa03808ecef
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-05-23 00:07:56 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -8,7 +8,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
-  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  url.search = new URLSearchParams({ ...searchParams, APPID: API_KEY });
 
   return fetch(url).then((res) => res.json());
 };
